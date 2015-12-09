c79c9e775860e6c18cf595dadb75f86e2eab2e98
dtg <tagedo74@gmail.com>
2015-12-09 20:01:20 +0300

    +

--- update_rating.php
+++ update_rating.php
@@ -4,6 +4,5 @@ use App\Classes\Db;
 
 $configDb = json_decode(file_get_contents(__DIR__ . '/config.json'));
 $db = new Db($configDb);
-
 $sql = 'UPDATE upwork SET rating = :rating WHERE id = :id';
-$db->dbExecute($sql, [':rating' => $_POST['rating'], ':id' => $_POST['id']]);
+$db->dbExecute($sql, [':rating' => $_GET['rating'], ':id' => $_GET['id']]);
