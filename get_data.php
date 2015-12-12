ddc3b1005b8505c8ff9b45ff3d7cc9ea507dd77c
dtg <tagedo74@gmail.com>
2015-12-12 19:27:20 +0300

    +

--- get_data.php
+++ get_data.php
@@ -12,10 +12,6 @@ foreach ($result['data'] as $value) {
     $created_at['display'] = date('d-m-Y H:i:s', $value->created_at);
     $created_at['timestamp'] = $value->created_at;
     $value->created_at = $created_at;
-
-    $rating['display'] = $value->rating;
-    $rating['sorting'] = $value->rating;
-    $value->rating = $rating;
 }
 
 echo json_encode($result, JSON_UNESCAPED_UNICODE);
